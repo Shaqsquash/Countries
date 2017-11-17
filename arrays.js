675ad2e38e96e395e1b50f991b98a7316f19c588
Eric <ericv53128@students.laalliance.org>
2017-11-17 17:21:43 +0000

    done

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var fourLetterCountries = [
+    "Chad",
+    "Cuba",
+    "Greenland", 
+    "Iraq",
+    "Mali",
+    "Oman",
+    "India"
+    
+    ]
+fourLetterCountries.push("Fiji")
+fourLetterCountries.pop("India")
+console.log(fourLetterCountries)
---
+++ countries/countries.js
@@ -0,0 +1,21 @@
+var Countries = [
+    "Chad",
+    "Cuba",
+    "Greenland", 
+    "Iraq",
+    "Mali",
+    "Oman",
+    "India"
+    
+    ]
+   
+    Countries.push("Fiji")
+    
+    Countries.splice(4,0, 'Iran')
+    
+    Countries.pop("India")
+    
+    Countries.reverse()
+    
+    Countries.splice(5,1, ' Tago')
+console.log(Countries)
